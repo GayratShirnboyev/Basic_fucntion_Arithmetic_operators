7c9601095c776c108e719101f560da84fe0c5efd
G'ayrat <shirnboyevgayrat@gmail.com>
2022-06-20 12:24:11 +0500

    file

--- bin/arithmetic01.dart
+++ bin/arithmetic01.dart
@@ -2,7 +2,10 @@
 // Create a function argument x of type int.
 // Create a parameter int type y of type int.
 // return sum of two parameters.
+int add(x, y) {
+  return x + y;
+}
 
 void main() {
-
+  print(add(3, 5));
 }
